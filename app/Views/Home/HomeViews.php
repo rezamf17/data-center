89811c07c6f113d8bf3c7c5f310d8dede5049246
reza.muhamad <reza.muhamad@pactindo.com>
2023-04-16 21:34:51 +0700

    tampilan image

--- app/Views/Home/HomeViews.php
+++ app/Views/Home/HomeViews.php
@@ -6,19 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?php echo base_url('tailwind/output.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('tailwind/vanilla.css'); ?>" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css">
-        <!-- TAILWIND CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.1.2/tailwind.min.css">
-        <!-- TAILWIND CSS UTILIZE  -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.1.2/utilities.min.css">
-        <!-- TAILWIND CSS COMPONENTS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.1.2/components.min.css">
-        <!-- TAILWIND CSS BASE -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.1.2/base.min.css">
+        <script src="https://cdn.tailwindcss.com"></script>
     <title>WIKA - Data Center</title>
 </head>
 <body>
-<div class=" background-login">
+<div class="background-login" style="height : 120vh;">
     <div class="mr-10 float-right">
             <div class="max-w-sm rounded overflow-hidden shadow-lg rounded-lg bg-white mt-20 px-8 py-4">
             <div class="px-6 py-4">
@@ -42,6 +34,60 @@
                 </button>
             </div>
     </div>
+</div>
+<div class="background-tamansari-skyhive h-screen">
+    <!-- <div class="max-w-sm overflow-hidden mt-20 px-8 py-4 text-white opacity-10 bg-black">
+      <div class="content-none"> 
+        <h2>Tamansari SkyHive</h2>
+        <h4>Jakarta, Indonesia</h4>
+      </div>
+    </div> -->
+<div class="opacity-10 w-5/12">
+  <div class="px-6 py-4 h-screen bg-black" style="background: linear-gradient(to left, rgba(169,208,113,0),rgba(0,0,0,9)); ">
+  <div class="py-20 px-10">
+      <h1 class="text-white font-bold text-xl">Taman Sari SkyHive</h1>
+      <p class="text-white">
+      Tamansari Skyhive Apartment Jakarta merupakan hunian eksklusif persembahan terbaru WIKA Realty bekerja sama dengan Medialand Group yang berada di tengah Jakarta’s Newest Hub – Cawang, Jakarta Timur. Apartemen ini dibangun di atas lahan seluas 5.331 m2 dan memiliki 571 unit yang terdiri dari 3 tipe unit, yaitu tipe studio (25-30 m2), tipe 1 bedroom (38-44 m2), dan tipe 2 bedroom (65-81 m2) dengan harga mulai kisaran Rp 30juta per meter persegi. 
+      </p>
+  </div>
+  </div>
+</div>
+</div>
+<div class="h-screen bg-black">
+  <div class="text-white object-none object-bottom" style="
+    position: absolute;
+    bottom: -150%;
+    width:100%;
+    height:100px;
+">
+    <div class="grid grid-rows-3 grid-flow-col gap-4">
+      <div class="row-span-3">
+        <img class="object-cover h-45 w-80" src="<?php echo base_url('img/Hadjar-Seti-Adji-large-removebg-preview.png'); ?>">
+        Hadjar Seti Adji
+        Director of Human Capital and Development
+      </div>
+      <div class="row-span-3">
+      <img class="object-cover h-45 w-80" src="<?php echo base_url('img/Hadjar-Seti-Adji-large-removebg-preview.png'); ?>">
+          Adityo Kusumo
+          Director of Finance and Risk Management
+      </div>
+      <div class="row-span-3">
+      <img class="object-cover h-45 w-80" src="<?php echo base_url('img/Hadjar-Seti-Adji-large-removebg-preview.png'); ?>">
+        Agung Budi Waskito
+        President Director
+      </div>
+      <div class="row-span-3">
+      <img class="object-cover h-45 w-80" src="<?php echo base_url('img/Hadjar-Seti-Adji-large-removebg-preview.png'); ?>">
+          Hananto Aji
+          Director of Operation I
+      </div>
+      <div class="row-span-3">
+      <img class="object-cover h-45 w-80" src="<?php echo base_url('img/Hadjar-Seti-Adji-large-removebg-preview.png'); ?>">
+      Ayu Widya Kiswari
+Director of Quality, Health, Safety and Environment
+      </div>
+</div>
+  </div>
 </div>
 </body>
 </html>
