ac43f87c959246aab6af388b1aa40c6e8fedd9d7
reza.muhamad <reza.muhamad@pactindo.com>
2023-07-01 15:23:35 +0700

    validasi gambar

--- app/Views/KelolaDataProyek/EditDokumen.php
+++ app/Views/KelolaDataProyek/EditDokumen.php
@@ -25,10 +25,8 @@
                     <a class="btn btn-success" onclick="openTab1()" href="#">Lihat</a>
                 <?php elseif (substr($fileProyek[0]->nama_file, -3) == 'xls' || substr($fileProyek[0]->nama_file, -4) == 'xlsx'): ?>
                     <a class="btn btn-secondary" href="#">Download</a>
-                <?php else: ?>
-                    <p>Format file tidak didukung atau file tidak ada</p>
-                <?php endif; ?>
-                <video controls width="250">
+                <?php elseif (substr($fileProyek[0]->nama_file, -3) == 'mp4' || substr($fileProyek[0]->nama_file, -3) == 'mkv'): ?>
+                    <video controls width="250">
                     <source src="<?php echo $fileProyek[0]->nama_file; ?>" type="video/webm">
 
                     <source src="/Uploads/<?php echo $fileProyek[0]->nama_file; ?>" type="video/mp4">
@@ -39,6 +37,11 @@
                     <a href="<?php echo $fileProyek[0]->nama_file; ?>">MP4</a>
                     video.
                 </video>
+                <?php elseif (substr($fileProyek[0]->nama_file, -3) == 'jpg' || substr($fileProyek[0]->nama_file, -3) == 'png'): ?>
+                    <img src="/Uploads/<?php echo $fileProyek[0]->nama_file; ?>" alt="Gambar Dokumen 1" style="max-width : 20em;">
+                <?php else: ?>
+                    <p>Format file tidak didukung atau file tidak ada</p>
+                <?php endif; ?>
             </td>
             <td>
                 <label>Ganti File</label>
@@ -57,6 +60,20 @@
                     <a class="btn btn-success" onclick="openTab2()">Lihat</a>
                 <?php elseif (substr($fileProyek[1]->nama_file, -3) == 'xls' || substr($fileProyek[1]->nama_file, -4) == 'xlsx'): ?>
                     <a class="btn btn-secondary" href="<?php echo base_url('Uploads/'.$fileProyek[1]->nama_file); ?>">Download</a>
+                <?php elseif (substr($fileProyek[1]->nama_file, -3) == 'mp4' || substr($fileProyek[1]->nama_file, -3) == 'mkv'): ?>
+                    <video controls width="250">
+                    <source src="<?php echo $fileProyek[1]->nama_file; ?>" type="video/webm">
+
+                    <source src="/Uploads/<?php echo $fileProyek[1]->nama_file; ?>" type="video/mp4">
+
+                    Download the
+                    <a href="<?php echo $fileProyek[1]->nama_file; ?>">WEBM</a>
+                    or
+                    <a href="<?php echo $fileProyek[1]->nama_file; ?>">MP4</a>
+                    video.
+                </video>
+                <?php elseif (substr($fileProyek[1]->nama_file, -3) == 'jpg' || substr($fileProyek[1]->nama_file, -3) == 'png'): ?>
+                    <img src="/Uploads/<?php echo $fileProyek[1]->nama_file; ?>" alt="Gambar Dokumen 2" style="max-width : 20em;">
                 <?php else: ?>
                     <p>Format file tidak didukung atau file tidak ada</p>
                 <?php endif; ?>
@@ -78,6 +95,20 @@
                     <a class="btn btn-success" onclick="openTab2()">Lihat</a>
                 <?php elseif (substr($fileProyek[2]->nama_file, -3) == 'xls' || substr($fileProyek[2]->nama_file, -4) == 'xlsx'): ?>
                     <a class="btn btn-secondary" href="<?php echo base_url('Uploads/'.$fileProyek[2]->nama_file); ?>">Download</a>
+                <?php elseif (substr($fileProyek[2]->nama_file, -3) == 'mp4' || substr($fileProyek[2]->nama_file, -3) == 'mkv'): ?>
+                    <video controls width="250">
+                    <source src="<?php echo $fileProyek[2]->nama_file; ?>" type="video/webm">
+
+                    <source src="/Uploads/<?php echo $fileProyek[2]->nama_file; ?>" type="video/mp4">
+
+                    Download the
+                    <a href="<?php echo $fileProyek[2]->nama_file; ?>">WEBM</a>
+                    or
+                    <a href="<?php echo $fileProyek[2]->nama_file; ?>">MP4</a>
+                    video.
+                </video>
+                <?php elseif (substr($fileProyek[2]->nama_file, -3) == 'jpg' || substr($fileProyek[2]->nama_file, -3) == 'png'): ?>
+                    <img src="/Uploads/<?php echo $fileProyek[2]->nama_file; ?>" alt="Gambar Dokumen 3" style="max-width : 20em;">
                 <?php else: ?>
                     <p>Format file tidak didukung atau file tidak ada</p>
                 <?php endif; ?>
