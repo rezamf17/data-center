e63a03e4e62d12e1f97785050249edaf668ed240
reza.muhamad <reza.muhamad@pactindo.com>
2023-08-19 22:12:19 +0700

    update validasi homeviews

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,15 @@ class Home extends BaseController
 {
     public function index()
     {
+        $session = session();
+
+        // Cek apakah pengguna sudah login dan memiliki sesi
+        if ($session->get('isLoggedIn') == 1) {
+            // Pengguna sudah login, mungkin Anda ingin mengarahkan mereka ke halaman lain
+            return redirect()->to('dashboard');
+        }
+    
+        // Jika pengguna belum login, tampilkan halaman HomeViews
         return view('Home/HomeViews');
     }
 }
